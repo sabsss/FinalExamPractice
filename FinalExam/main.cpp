aa1a19a10e08de1bc7a3dae8b35691a1b4acbd30
Siba <mohamed.s1@live.unic.ac.cy>
2021-05-27 19:17:38 +0400

    Initial Commit

---
+++ FinalExam/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FinalExam
+//
+//  Created by Siba Dan on 27/05/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
