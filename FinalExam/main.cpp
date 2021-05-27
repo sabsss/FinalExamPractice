423588e232bb5f3d753e4e5139c17f29aa909f24
Siba <mohamed.s1@live.unic.ac.cy>
2021-05-27 20:31:03 +0400

    Made the skeleton of the program

--- FinalExam/main.cpp
+++ FinalExam/main.cpp
@@ -1,14 +1,54 @@
-//
-//  main.cpp
-//  FinalExam
-//
-//  Created by Siba Dan on 27/05/2021.
-//
-
+/** \file FinalExam.cpp
+ *  \brief small math program
+ *  \author Siba Omar Almohamed
+ *  \version 0.1
+ *  \date 27/5/2021
+ *  \copyright UNIC
+*/
 #include <iostream>
+#include <cassert>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//Function Prototypes
+int sumOfRange(int startVal, int endVal);
+int productOfRange(int startVal, int endVal);
+bool isPositive(double value);
+/**
+ * The driver of the cpp. presents the menu to the user and executes the requests
+ * @return returns 0.
+ */
+int main()
+{
     return 0;
+}
+
+/**
+ * The <code>sumOfRange</code> function finds the sum
+ * @param startVal The starting value.
+ * @param endVal The ending value.
+ * @return returns the sum of the range.
+ */
+int sumOfRange(int startVal, int endVal)
+{
+    return 0;
+}
+
+/**
+ * The <code>productOfRange</code> function finds the product of
+ * @param startVal The starting value.
+ * @param endVal The ending value.
+ * @return returns the product of the range.
+ */
+int productOfRange(int startVal, int endVal)
+{
+    return 0;
+}
+/**
+ * The <code>isPositive</code> function checks if given number is positive.
+ * @param value The value that will be checked if positive or not.
+ * @return returns true if the value is positive and false if it is negative.
+ */
+bool isPositive(double value)
+{
+    return true;
 }
